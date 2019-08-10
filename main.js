9e84c548749aec7e6aaff2854cabed3cce1cbc79
Raphael Rocha <raphaelrochabcc@gmail.com>
2019-08-10 14:55:39 -0300

    Quinta aula

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
+const soma = (a = 3, b = 6) => a + b
+
+console.log(soma(1));
+console.log(soma());
+
+
+//Quinta aula
+// const arr = [1, 3, 4, 5, 6];
+
+// const newArr = arr.map(item => item * 2);  
+
+// console.log(newArr);
+
+// const teste = () => ({nome: 'Diego'});
+// console.log(teste());
 
 //Quarta aula 
 // const arr = [1, 3, 4, 5, 8, 9];
