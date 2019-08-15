0c63a7e7965ba59e10937806991b135b131fac3b
Raphael Rocha <raphaelrochabcc@gmail.com>
2019-08-15 20:48:53 -0300

    Render OK

--- src/main.js
+++ src/main.js
@@ -1,17 +1,53 @@
-import axios from 'axios';
+class App {
+    constructor() {
+        this.repositories = [];
 
-class Api{
-     static async getUserInfo(username){
-        try {
-            const response = await axios.get(`https://api.github.com/users/${username}`);
-            console.log(response);
-        } catch (error) {
-            console.warn('Erro na API')
-        }
-        
-        
-        
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+
+        this.registerHandlers();
+    }
+    registerHandlers() {
+       this.formEl.onsubmit = event => this.addRepository(event);
+       
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274',
+            html_url: 'http://github.com/rocketseat/rocketseat.com.br',
+        });
+        this.render();
+    }
+    render(){
+        this.listEl.innerHTML = '';
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
     }
 }
-Api.getUserInfo('RaphaelSR');
-Api.getUserInfo('RaphaelSRdsd');
+
+new App();
